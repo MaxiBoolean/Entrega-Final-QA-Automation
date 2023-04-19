7c65683ac346e5412d18e15065875c23cad9385e
Maximiliano Cordone <maxicordone@hotmail.com>
2023-04-19 17:52:49 -0300

    Cambios en pags

--- cypress/support/pages/productPage.js
+++ cypress/support/pages/productPage.js
@@ -1,5 +1,13 @@
 export class ProductPage {
+  constructor() {
+    this.modalButton = "#closeModal";
+    this.goShoppingCartButton = "#goShoppingCart";
+  }
   agregarProducto(producto) {
-    return cy.contains('p', producto).siblings("button").click();
+    return cy.contains("p", producto).siblings("button").click();
+  }
+
+  botonClick(button) {
+    return cy.get(button).should("be.visible").click();
   }
 }
